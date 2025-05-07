04d854283fb5ccca19d0186e97b205d4955ea844
Kazimiire <redmond318@gmail.com>
2025-05-07 07:35:32 -0400

    updated main.py to open and print book contents

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
+        book_contents = f.read()
+    return book_contents
+
+def main():
+    book_contents = get_book_text("books/frankenstein.txt")
+    print(book_contents)
+
+
+if __name__ == "__main__":
+    main()
